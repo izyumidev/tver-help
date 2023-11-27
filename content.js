eb836c288186a9d95ed9bd34f8e5ae336feedc26
Yumi Izumi <mail@yumi.to>
2023-11-27 13:30:42 -0500

    Add files via upload

--- content.js
+++ content.js
@@ -1,12 +1,16 @@
 window.browser = window.msBrowser || window.browser || window.chrome;
 
-function getLink() {
+function getLink(withcheck = false) {
 	const acc = $(".vod-player_videoContainer__YYjtt .video-js").attr(
 		"data-account"
 	);
 	const id = $(".vod-player_videoContainer__YYjtt .video-js").attr(
 		"data-video-id"
 	);
+	if (withcheck && (!acc || !id)) {
+		alert("No video found");
+		return;
+	}
 	const link =
 		"http://players.brightcove.net/" +
 		acc +
@@ -17,6 +21,11 @@ function getLink() {
 	return link;
 }
 
-window.addEventListener("load", () => getLink(), false);
-
 window.addEventListener("click", () => getLink());
+
+window.addEventListener("keydown", (event) => {
+	if (event.key === "b" && event.shiftKey && event.metaKey) {
+		const link = getLink(true);
+		navigator.clipboard.writeText(link);
+	}
+});
